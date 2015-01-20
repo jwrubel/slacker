e23bbb38654199b6ce4cb6714300f4c2e9e096ca
Nathan Hoad <nathan@nathanhoad.net>
2015-01-20 10:21:12 +1000

    Changed the link to get your Slack API token

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var bower_components = {
     './bower_components/klass/klass.js'
   ],
   css: [
-    './bower_components/bootstrap-css/css/bootstrap.css'
+    './bower_components/bootstrap/css/bootstrap.css'
   ]
 };
 
